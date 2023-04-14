40e325990f58f82a4189accff3fabb4fd8421cfb
Bognanna <101549864+Bognanna@users.noreply.github.com>
2023-04-14 20:16:53 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,15 +31,15 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
+#exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
