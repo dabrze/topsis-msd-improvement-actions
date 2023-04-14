d3bc0fc24c79191be159959f65a0a77ba4c8687f
Bognanna <101549864+Bognanna@users.noreply.github.com>
2023-04-14 18:39:56 +0000

    Update html_theme in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ templates_path = ['_templates']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 #html_static_path = ['_static']
 
 # -- Options for EPUB output
