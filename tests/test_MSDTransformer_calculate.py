ee2609771840fa919800e1f9059b15f99d0fe871
MaciejWos01 <87046486+MaciejWos01@users.noreply.github.com>
2023-04-17 20:35:11 +0200

    Implementation of parametrizing fixtures

--- tests/test_MSDTransformer_calculate.py
+++ tests/test_MSDTransformer_calculate.py
@@ -40,58 +40,18 @@ def test_calculate_normData(df):
     buses.fit(df, objectives=objectives)
     buses.transform()
     assert (buses.data.all().all() <= 1 and buses.data.all().all() >= 0)
-
-def test_calculate_mean(df):
-    objectives = ['max','max','min','max','min','min','min','max']
-    buses = MSDTransformer()
-    buses.fit(df, objectives=objectives)
-    buses.transform()
-    result = buses.data['Mean'][:5].values.tolist()
-    result = [round(num, 6) for num in result]
-    result2 = [0.8471063335888918, 0.7768981460905122, 0.49858239165620366, 0.8586295634766312, 0.18392857142857144]
-    result2 = [round(num, 6) for num in result2]
-    assert result == result2
-
-def test_calculate_sd(df):
-    objectives = ['max','max','min','max','min','min','min','max']
-    buses = MSDTransformer()
-    buses.fit(df, objectives=objectives)
-    buses.transform()
-    result = buses.data['Std'][:5].values.tolist()
-    result = [round(num, 6) for num in result]
-    result2 = [0.10893897542978036, 0.11494599992993791, 0.22042413986540876, 0.10427543268417871, 0.3289933238575779]
-    result2 = [round(num, 6) for num in result2]
-    assert result == result2
     
-def test_calculate_topsisI(df):
+@pytest.mark.parametrize("col_name, agg_fn, expected", [('Mean', 'I', [0.8471063335888918, 0.7768981460905122, 0.49858239165620366, 0.8586295634766312, 0.18392857142857144]),
+                                                        ('Std', 'I', [0.10893897542978036, 0.11494599992993791, 0.22042413986540876, 0.10427543268417871, 0.3289933238575779]), 
+                                                        ('AggFn', 'I', [0.8122656834877503, 0.7490278499160838, 0.452271582449278, 0.8243327970727589, 0.12010842504688712]),
+                                                        ('AggFn', 'A', [0.8540824554889914, 0.7853555324174957, 0.5451341144205023, 0.8649382019182312, 0.37691686952246145]),
+                                                        ('AggFn', 'R', [0.8198010230228603, 0.757825488673488, 0.4988130697803592, 0.8311874970961471, 0.29990001354560974])])
+def test_calculate_values(col_name, agg_fn, expected, df):
     objectives = ['max','max','min','max','min','min','min','max']
-    buses = MSDTransformer('I')
+    buses = MSDTransformer(agg_fn)
     buses.fit(df, objectives=objectives)
     buses.transform()
-    result = buses.data['AggFn'][:5].values.tolist()
+    result = buses.data[col_name][:5].values.tolist()
     result = [round(num, 6) for num in result]
-    result2 = [0.8122656834877503, 0.7490278499160838, 0.452271582449278, 0.8243327970727589, 0.12010842504688712]
-    result2 = [round(num, 6) for num in result2]
-    assert result == result2
-
-def test_calculate_topsisA(df):
-    objectives = ['max','max','min','max','min','min','min','max']
-    buses = MSDTransformer('A')
-    buses.fit(df, objectives=objectives)
-    buses.transform()
-    result = buses.data['AggFn'][:5].values.tolist()
-    result = [round(num, 6) for num in result]
-    result2 = [0.8540824554889914, 0.7853555324174957, 0.5451341144205023, 0.8649382019182312, 0.37691686952246145]
-    result2 = [round(num, 6) for num in result2]
-    assert result == result2
-
-def test_calculate_topsisR(df):
-    objectives = ['max','max','min','max','min','min','min','max']
-    buses = MSDTransformer('R')
-    buses.fit(df, objectives=objectives)
-    buses.transform()
-    result = buses.data['AggFn'][:5].values.tolist()
-    result = [round(num, 6) for num in result]
-    result2 = [0.8198010230228603, 0.757825488673488, 0.4988130697803592, 0.8311874970961471, 0.29990001354560974]
-    result2 = [round(num, 6) for num in result2]
+    result2 = [round(num, 6) for num in expected]
     assert result == result2
