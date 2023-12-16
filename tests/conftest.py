e39e43ba347034b069a7aa21c6b020b1e1e5443d
Bognanna <boginidragon@gmail.com>
2023-12-16 14:12:20 +0100

    New file name in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import pandas as pd
 
 @pytest.fixture(autouse=True)
 def df():
-    data = pd.read_csv("data/bus.csv", sep=";", index_col=0)
+    data = pd.read_csv("data/bus_original.csv", sep=";", index_col=0)
     return data
